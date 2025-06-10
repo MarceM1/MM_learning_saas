5cfeee21f58a9edd71e6c5699c12aee87a50d1cc
Marcelo Melogno <marcelomelogno1986@gmail.com>
2025-06-09 23:30:38 -0300

    Implement Companion page & CompanionComponent

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,30 @@ import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import Cta from '@/components/CTA'
 import { recentSessions } from '@/constants'
+import { getAllCompanions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+
   return (
     <main>
       <h1 className="text-2xl font-bold">Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard 
-          id='123'
-          name='Neura the Brain Explorer'
-          topic='Neural Network of the Brain'
-          subject='science'
-          duration={45}
-          color='#ffda6e'
-        />
-        <CompanionCard 
-          id='456'
-          name='Countsy the Number Wizard'
-          topic='Derivatives & Integrals'
-          subject='Maths'
-          duration={30}
-          color='#e5d0ff'
-        />
-        <CompanionCard 
-          id='798'
-          name='Verbal the Vocabulary Builder'
-          topic='English Literature'
-          subject='Language'
-          duration={30}
-          color='#8de7ff'
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
       <section className="home-section">
-        <CompanionsList 
+        <CompanionsList
           title="Recently completed sessions"
-          companions = {recentSessions}
-          classNames = 'w-2/3 max-lg:w-full'
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full'
         />
         <Cta />
       </section>
